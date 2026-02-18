24a3c01b89462c6820a4d212b49a33b17023c1f3
kshitij kardile <kshitijkardile@gmail.com>
2026-02-18 12:17:43 +0530

    swagger token auth

--- backend/app/core/rbac.py
+++ backend/app/core/rbac.py
@@ -1,18 +1,22 @@
 # app/core/rbac.py
 from fastapi import Depends, HTTPException
-from jose import jwt
-from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from app.core.jwt import SECRET_KEY, ALGORITHM
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+auth_scheme = HTTPBearer()
 
 def require_role(allowed_roles: list[str]):
-    def role_checker(token: str = Depends(oauth2_scheme)):
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        role = payload.get("role")
+    def role_checker(auth: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+        token = auth.credentials
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            role = payload.get("role")
 
-        if role not in allowed_roles:
-            raise HTTPException(status_code=403, detail="Access forbidden")
-        return payload
+            if role not in allowed_roles:
+                raise HTTPException(status_code=403, detail="Access forbidden")
+            return payload
+        except JWTError:
+            raise HTTPException(status_code=401, detail="Invalid token")
 
     return role_checker
